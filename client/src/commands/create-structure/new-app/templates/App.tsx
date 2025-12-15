a63c135efaa9b3d7972d678b9b59021bb1430fb0
fhidalgoGC <111799320+fhidalgoGC@users.noreply.github.com>
2025-12-15 20:20:28 +0000

    w

--- client/src/commands/create-structure/new-app/templates/App.tsx
+++ client/src/commands/create-structure/new-app/templates/App.tsx
@@ -3,24 +3,14 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home}/>
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import AppRoutes from "@/routes";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <AppRoutes />
       </TooltipProvider>
     </QueryClientProvider>
   );
